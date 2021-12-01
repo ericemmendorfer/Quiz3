1ef4cbf005a27ea8ffedbaefbc48e30a4df73110
ericemmendorfer <91583186+ericemmendorfer@users.noreply.github.com>
2021-12-01 16:53:20 -0500

    Update TicTacToe.java

--- TicTacToe.java
+++ TicTacToe.java
@@ -2,6 +2,46 @@ import java.util.Scanner;
 
 public class TicTacToe{
 	public  static void main(String[] args){
+	int [][] board= {{1,2,3},{4,5,6},{7,8,9}};
+		
+new Scanner=sc.Scanner
+
+System.out.println("Enter 1 to play with 2 people, 2 to play vs the CPU, and 0 to quit");
+int play= sc.nextInt();
+if (play==0){
+	break:
+}
+else if(play=1){
+}
+else if (play=2){
+}
+		
+		
+		
+		//create board here and print 
+		
+		
+		//while statements to check game is not over
+while ( board[0][0]==board[0][1]==board[0][2]=false){
+while ( board[1][0]==board[1][1]==board[1][2]=false){
+while ( board[2][0]==board[2][1]==board[2][2]=false){
+while ( board[0][0]==board[1][0]==board[2][0]=false){
+while ( board[0][1]==board[1][1]==board[2][1]=false){
+while ( board[0][2]==board[1][2]==board[2][2]=false){
+while ( board[0][0]==board[1][1]==board[2][2]=false){
+while ( board[0][2]==board[1][1]==board[2][0]=false){
+	
+
+		
+}
+}
+}
+}
+}	
+}		
+}
+}
+	
 
 		
 }
