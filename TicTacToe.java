1e1cf5264bd6edaf2be473829b5b61f5f73977d5
ericemmendorfer <91583186+ericemmendorfer@users.noreply.github.com>
2021-12-02 13:20:55 -0500

    Update TicTacToe.java

--- TicTacToe.java
+++ TicTacToe.java
@@ -25,25 +25,36 @@ public class TicTacToe {
 		int [] arr= {0,0,0,0,0,0,0,0,0};
 
 		//while statements to check game is not over
-while ( board[0][0]==board[0][1]==board[0][2]=false){
-while ( board[1][0]==board[1][1]==board[1][2]=false){
-while ( board[2][0]==board[2][1]==board[2][2]=false){
-while ( board[0][0]==board[1][0]==board[2][0]=false){
-while ( board[0][1]==board[1][1]==board[2][1]=false){
-while ( board[0][2]==board[1][2]==board[2][2]=false){
-while ( board[0][0]==board[1][1]==board[2][2]=false){
-while ( board[0][2]==board[1][1]==board[2][0]=false){
-	
+
+public static String checkBoard(){	
+if ( board[0]==board[1]==board[2]){
+	System.out.println("You win!");
+}
+else if ( board[3]==board[4]==board[5]){
+	System.out.println("You win!");
+}
+else if ( board[6]==board[7]==board[8]){
+	System.out.println("You win!");
+}
+else if ( board[0]==board[3]==board[6]){
+	System.out.println("You win!");
+}
+else if ( board[1]==board[4]==board[7]){
+	System.out.println("You win!");
+}
+else if ( board[2]==board[5]==board[8]){
+	System.out.println("You win!");
+}
+else if ( board[0]==board[4]==board[8]){
+	System.out.println("You win!");
+}
+else if ( board[2]==board[4]==board[6]){
+	System.out.println("You win!");
+}
+}
 
 		
-}
-}
-}
-}
-}	
-}		
-}
-}
+
 	
 
 		
