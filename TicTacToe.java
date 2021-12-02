8747904f52505432c8c03efadb0b9b11bb0ad7f0
nicholaschoi1115 <94401387+nicholaschoi1115@users.noreply.github.com>
2021-12-02 12:59:02 -0500

    added input section

--- TicTacToe.java
+++ TicTacToe.java
@@ -1,22 +1,24 @@
 import java.util.Scanner;
 
-public class TicTacToe{
-	public  static void main(String[] args){
-	int [][] board= {{1,2,3},{4,5,6},{7,8,9}};
-		
-new Scanner=sc.Scanner
-
-System.out.println("Enter 1 to play with 2 people, 2 to play vs the CPU, and 0 to quit");
-int play= sc.nextInt();
-if (play==0){
-	break:
-}
-else if(play=1){
-}
-else if (play=2){
-}
-		
-		
+public class TicTacToe {
+	public  static void main(String[] args) {
+		int [][] board= {{1,2,3},{4,5,6},{7,8,9}};
+		Scanner sc = new Scanner(System.in);
+		int play = 3;
+	do {
+		System.out.println("Enter 1 to play with 2 people, 2 to play vs the CPU, and 0 to quit");
+		play= sc.nextInt();
+		switch(play) {
+			case 0: 
+				System.out.println("bye"); 
+				System.exit(0);
+				break;
+			case 1: System.out.println("CPU"); break;
+			case 2: System.out.println("hi"); break;
+			default:
+				System.out.println("Invalid input, please try again"); 
+		}
+		} while(play < 0 || play > 2);
 		
 		//create board here and print 
 		
