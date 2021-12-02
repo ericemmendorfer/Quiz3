6784a5a407e13e9fc10b3ad47fcf6e50b61ce7c9
nicholaschoi1115 <94401387+nicholaschoi1115@users.noreply.github.com>
2021-12-02 13:42:47 -0500

    fixed checkboard

--- TicTacToe.java
+++ TicTacToe.java
@@ -116,30 +116,23 @@ if (turn==10){
 			}
 		}
 	}
-	public static String checkBoard(){	
-if ( board[0]==board[1]==board[2]){
-	System.out.println("You win!");
-}
-else if ( board[3]==board[4]==board[5]){
-	System.out.println("You win!");
-}
-else if ( board[6]==board[7]==board[8]){
-	System.out.println("You win!");
-}
-else if ( board[0]==board[3]==board[6]){
-	System.out.println("You win!");
-}
-else if ( board[1]==board[4]==board[7]){
-	System.out.println("You win!");
-}
-else if ( board[2]==board[5]==board[8]){
-	System.out.println("You win!");
-}
-else if ( board[0]==board[4]==board[8]){
-	System.out.println("You win!");
-}
-else if ( board[2]==board[4]==board[6]){
-	System.out.println("You win!");
-}
+	public static String checkBoard(int[] board){	
+		if (board[0]==board[1] && board[0]==board[2])
+			System.out.println("You win!");
+		else if ( board[3]==board[4] && board[4]==board[5])
+			System.out.println("You win!");
+		else if ( board[6]==board[7] && board[6]==board[8])
+			System.out.println("You win!");
+		else if ( board[0]==board[3] && board[0]==board[6])
+			System.out.println("You win!");
+		else if ( board[1]==board[4] && board[1]==board[7])
+			System.out.println("You win!");
+		else if ( board[2]==board[5] && board[2]==board[8])
+			System.out.println("You win!");
+		else if ( board[0]==board[4] && board[0]==board[8])
+			System.out.println("You win!");
+		else if ( board[2]==board[4] && board[2]==board[6])
+			System.out.println("You win!");
+	}
 }
 }
